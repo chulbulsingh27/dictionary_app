bcc855f8609207224562f4701f6b7cce152de159
Chulbul Singh <66539055+chulbulsingh27@users.noreply.github.com>
2022-05-18 17:29:11 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,108 @@
+import axios from "axios";
+import './App.css';
+import { useEffect, useState } from "react";
+import { alpha, Container, styled, Switch } from "@material-ui/core";
+import Header from "./components/Header/Header";
+import Definitions from "./components/Definitions/Definitions";
+import { grey } from "@material-ui/core/colors";
+import { withStyles } from "@material-ui/styles";
+function App() {
+
+  const [word, setWord] = useState("");
+  const [meanings, setMeanings] = useState([]);
+  const [category, setCategory] = useState("en");
+  const [LightMode, setLightMode] = useState(false);
+  const [LightTheme, setLightTheme] = useState(false);
+
+
+
+  const PurpleSwitch = withStyles({
+    switchBase: {
+      color: grey[50],
+      "&$checked": {
+        color: grey[900],
+      },
+      "&$checked + $track": {
+        backgroundColor: grey[500],
+      },
+    },
+    checked: {},
+    track: {},
+  })(Switch);
+
+
+
+
+
+
+
+  const dictionaryApi = async () => {
+    try {
+      const data = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/${category}/${word}`);
+      //console.log(data);
+      setMeanings(data.data)
+
+    } catch (error) {
+      console.log(error);
+
+    }
+
+  };
+  console.log(meanings);
+  useEffect(() => {
+    dictionaryApi();
+  }, [word, category]);
+
+  return (
+    <div
+      className="App"
+      style={{
+        height: "100vh",
+        backgroundColor: LightTheme ? "#fff" : "#282c34",
+        color: LightTheme ? "black" : "white",
+        transition: "all 0.5s linear",
+      }}
+    >
+      <Container
+        maxWidth="md"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          height: "100vh",
+          justifyContent: "space-evenly",
+        }}
+      >
+
+        <div
+          style={{ position: "absolute", top: 0, right: 15, paddingTop: 10 }}
+        >
+          <span>{LightTheme ? "Dark" : "Light"} Mode</span>
+          <PurpleSwitch
+            checked={LightTheme}
+            onChange={() => setLightTheme(!LightTheme)}
+          />
+        </div>
+
+        <Header
+          setWord={setWord}
+          category={category}
+          setCategory={setCategory}
+          word={word}
+          setMeanings={setMeanings}
+          LightTheme={LightTheme}
+        />
+        {meanings && (
+          <Definitions
+            meanings={meanings}
+            word={word}
+            LightTheme={LightTheme}
+            category={category}
+          />
+        )}
+      </Container>
+
+    </div>
+  );
+}
+
+export default App;
